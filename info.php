47381c9154c49d0903059bb9ca7f5974a6a63948
labby <rp@cms-lab.com>
2016-07-01 12:01:20 +0200

    modify license

--- info.php
+++ info.php
@@ -6,23 +6,23 @@
  *	@version		see info.php of this module
  *	@authors		David Ilicz Klementa, Burkhard Hekers, Jurgen Nijhuis, John Maats,erpe
  *	@copyright		2012-2016 David Ilicz Klementa, Burkhard Hekers, Jurgen Nijhuis, John Maats,erpe
- *  @license        GNU General Public License
- *  @license terms  see info.php of this module
- *  @platform       see info.php of this module
+ *	@license		GNU General Public License
+ *	@license terms	see info.php of this module
+ *	@platform		see info.php of this module
  *
  *	Based on MyCalendar by Burkhard Hekers
  */
 
 
 
-$module_directory   = 'procalendar';
-$module_name        = 'ProCalendar';
-$module_function    = 'page';
-$module_version     = '1.4.0';
-$module_platform 	= '2.x';
-$module_author      = 'David Ilicz Klementa, Burkhard Hekers, Jurgen Nijhuis, John Maats,erpe';
-$module_license		= '<a href="http://www.gnu.org/licenses/lgpl.html" target="_blank">lgpl</a>';
+$module_directory	= 'procalendar';
+$module_name		= 'ProCalendar';
+$module_function	= 'page';
+$module_version		= '1.4.0';
+$module_platform	= '2.x';
+$module_author		= 'David Ilicz Klementa, Burkhard Hekers, Jurgen Nijhuis, John Maats,erpe';
+$module_license		= '<a href="http://www.gnu.org/licenses/gpl-3.0-standalone.html" target="_blank">gnu gpl</a>';
 $module_license_terms	= '-';
 $module_description	= 'Event calendar';
-$module_home        = 'http://cms-lab.com';
+$module_home		= 'http://cms-lab.com';
 $module_guid		= '27a8d8b3-1857-4909-a932-97af33c22bda';
