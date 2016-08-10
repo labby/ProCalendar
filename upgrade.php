86dacf24a2b4652640306d869c26b827a7af1233
labby <rp@cms-lab.com>
2016-08-10 18:42:11 +0200

    header modification

--- upgrade.php
+++ upgrade.php
@@ -1,11 +1,23 @@
 <?php
 /*
- * CMS module: MPForm
- * For more information see info.php
- * 
- * upgrade.php provides the functions for an upgrade from an older version of the module.
- * This file is (c) 2009 Website Baker Project <http://www.websitebaker.org/>
- * Improvements are copyright (c) 2009-2011 Frank Heyne
+
+ Website Baker Project <http://www.websitebaker.org/>
+ Copyright (C) 2004-2006, Ryan Djurovich
+
+ Website Baker is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Website Baker is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Website Baker; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 */
 
 // Must include code to stop this file being access directly
@@ -114,3 +126,4 @@ if (!isset($fields['rec_exclude'])){
 }
 
 echo '<div class="info"><B>Module proclendar updated to version: 1.3</B></div>';
+echo '<div class="info"><B>Module proclendar updated to version: 1.4</B></div>';
